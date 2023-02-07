4ee8d6cf1ba8e6f4f1e476dc460c1d51f29ef9ae
Pierre <p.esseul@gmail.com>
2023-02-07 14:26:43 +0100

    Recup dans cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,45 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useShoppingCart } from 'use-shopping-cart';
-import { DebugCart } from 'use-shopping-cart'
+
+import {AiOutlinePlusCircle, AiOutlineMinusCircle} from 'react-icons/ai';
 
 import './Cart.css';
 
 const Cart = (props) => {
 
-    const { cartCount, formattedTotalPrice, currency, totalPrice, cartDetails} = useShoppingCart()
+    const { cartCount, currency, totalPrice, cartDetails, addItem, removeItem} = useShoppingCart()
 
     console.log("Value cartCount : ",cartCount)
     console.log("Value cartDetails : ", cartDetails)
     console.log("total price : ",totalPrice);
     console.log("Value currency : ", {currency});
+
+    const [quantity, setQuantity] = useState(0);
+    
+    function addQuantity(quantity, product) {
+        setQuantity(quantity++); 
+        addItem(product);
+    }
+
+    function removeQuantity(quantity, product, cartCount) {
+        setQuantity(quantity--);
+        removeItem(product);
+        console.log(product)
+    }
+
+    function payment() {
+        //TODO: redirection to STRIPE
+    }
     
     return (
-    <>
-      <h1>{totalPrice}</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Product</th>
-            <th>Quantity</th>
-            <th>Price</th>
-            <th>Total Price</th>
-          </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td>{totalPrice}</td>
-        </tr>
-        
-          {/* {cartDetails.map(item => (
-            <tr key={item.id}>
-              <td>{item.name_article}</td>
-              <td>{item.quantity}</td>
-              <td>{item.price}</td>
-            </tr>
-          ))} */}
-        </tbody>
-      </table>
-    </>
+    <div className='cart'>
+        {cartCount === 0 && (
+            <h3>Vous n'avez pas d'article</h3> 
+        )}  
+            
+            {Object.entries(cartDetails).map(([key, value]) => (
+                <div className="items" key={key}>
+                    <div><img src={value.picture_url} alt="product display" /></div>
+                    <div className='items__name'>{value.name_article}</div>
+                    <div className='items__quantity'>
+                        <button type='button' onClick={removeQuantity}><AiOutlineMinusCircle /></button>
+                        <div>{quantity}</div>
+                        <button type='button' onClick={addQuantity}><AiOutlinePlusCircle /></button>
+                    </div>
+                    <div className='items__price'>{value.price}€</div>
+                </div> 
+            ))}
+            
+            <div className='total__price'>
+                <div className='total'>Prix total :</div>
+                <div className='amount'>{totalPrice}€</div>
+            </div> 
+            
+            <div className='payment'>
+                <button type='button' onClick={payment} >Payer</button>
+            </div>
+    </div>
   
     )
 }
