9fa1fd941b2b5cb543a4590e0204936154addd41
Thomas Viot <viotthomas7@gmail.com>
2023-02-09 14:56:50 +0100

    popup caddie

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,19 +2,27 @@ import React from 'react';
 
 import {MdOutlinePermContactCalendar} from 'react-icons/md';
 import {AiOutlineShoppingCart} from 'react-icons/ai';
+import { useShoppingCart } from 'use-shopping-cart';
 
 
 import './Header.css';
 
 const Header = (props) => {
+    const {cartCount} = useShoppingCart();
     return(
         <div className='header'>
             <a href="/"><h1>Shop Name</h1></a>
-
+        
             <div className='icons'>
-                <a href="/cart"><AiOutlineShoppingCart /></a>
+                <a href="/cart">
+                    <AiOutlineShoppingCart />
+                    {cartCount !== 0 && (<div className='number'>
+                    <p>{cartCount}</p>
+            </div>)}
+                </a>
                 <a href="/contact"><MdOutlinePermContactCalendar /></a>
             </div>
+            
         </div>
     );
 };
