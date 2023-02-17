94d74a5cc5d937c136d82c569fe9a8b5598dc860
Thomas Viot <viotthomas7@gmail.com>
2023-02-17 10:23:19 +0100

    gestion quand pas de tel

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -31,24 +31,32 @@ const Contact = (props) => {
                                 <HiMail />
                                 <div>{props.data.user.mail}</div>
                             </div>
+                        </div>  
+                        
+                        
+                        {props.data.address.phone_number && (
+                        <>
                             <div className='phone'>
                                 <HiPhone />
                                 <div>{props.data.address.phone_number}</div>
                             </div>
-                        </div>
-                        <div className='address'>
-                            <p>Adresse :</p>
-                            <div className='address__details'>
-                                <div>{props.data.address.street}</div>
-                                <div>{props.data.address.postal_code}, {props.data.address.city}</div>
+                            
+                            <div className='address'>
+                                    <p>Adresse :</p>
+                                    <div className='address__details'>
+                                        <div>{props.data.address.street}</div>
+                                        <div>{props.data.address.postal_code}, {props.data.address.city}</div>
+                                    </div>
                             </div>
-                        </div>
-                        <div className='hours'>
-                            <p>Horaire d'ouverture :</p>
-                            <div className='hours__details'>
-                                <div>{props.data.address.hours}</div>
+
+                            <div className='hours'>
+                                <p>Horaire d'ouverture :</p>
+                                <div className='hours__details'>
+                                    <div>{props.data.address.hours}</div>
+                                </div>
                             </div>
-                        </div>
+                        </>)}
+
                     </div>
                     <div className='image'><img src='./img/png/cardPersonnalisez.png' alt='contact infos' /></div>
                 </div>
